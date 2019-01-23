6bd5e8c7fce8f45f16c5a53bd25d4d2d099710e2
Lidii <lidiiany92@gmail.com>
2019-01-22 21:05:23 -0500

    fetching weather object from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Titles from "./components/Titles.js";
+import Form from "./components/Form.js";
+import Weather from "./components/Weather.js";
+
+const api_key = "964cb9513e0ee480a2f32f64a4d6ce9f";
 
 class App extends React.Component {
+
+  getWeather = async (event) => {
+    event.preventDefault();
+
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=964cb9513e0ee480a2f32f64a4d6ce9f`);
+
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render(){
     return (
       <div>
         <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
